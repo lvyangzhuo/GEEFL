2b4ccb85e87cb71287df3b4c189728c6791267ce
lvyangzhuo@163.com <lvyangzhuo123>
2017-08-06 11:40:44 +0800

    create spring-boot project jeefl

---
+++ src/main/java/com/jeefl/JeeflApplication.java
@@ -0,0 +1,12 @@
+package com.jeefl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeflApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeeflApplication.class, args);
+	}
+}
---
+++ src/main/java/com/jeefl/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.jeefl.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by lyz on 2017-8-6.
+ */
+@RestController
+@EnableAutoConfiguration
+public class HelloWorldController {
+
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
